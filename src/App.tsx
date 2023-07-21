c0b918784e78d471b744684926b0075f153a6d5d
Rhys Stephens <rhys.s@logically.ai>
2023-07-21 16:37:47 +0100

    Basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function getTitle(title: string) {
+  return title
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+
+      {array.map((item) => {
+        return (
+          <div key={item}>
+            <span>{item}</span>
+          </div>
+        )
+      })}
+
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+    </div>
   )
 }
 
