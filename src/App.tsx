48f695c870fef133b770c2fd9dff1505095b447a
Rhys Stephens <rhys.s@logically.ai>
2023-09-15 14:56:56 +0100

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -120,8 +120,10 @@ const App = () => {
     setSearchTerm(event.target.value);
   }
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event: React.SyntheticEvent) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
   }
 
   const handleRemoveStory = (item: Story) => {
@@ -135,17 +137,7 @@ const App = () => {
     <>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={handleSearchInput}>
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+      <SearchForm searchTerm={searchTerm} onSearchInput={handleSearchInput} onSearchSubmit={handleSearchSubmit}/>
 
       <p>
         Searching for <strong>{searchTerm}</strong>.
@@ -229,5 +221,30 @@ const InputWithLabel = (
     />
   </>
 );
+
+type SearchFormProps = {
+  searchTerm: string,
+  onSearchInput: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  onSearchSubmit: (event: React.SyntheticEvent) => void
+}
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit
+}: SearchFormProps) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={onSearchInput}>
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button
+      type="submit"
+      disabled={!searchTerm}
+    >
+      Submit
+    </button>
+  </form>
+)
 
 export default App
