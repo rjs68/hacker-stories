aef306cbe606f6f0b67486e8a69e84b067dc7e27
Rhys Stephens <rhys.s@logically.ai>
2023-09-15 14:39:34 +0100

    3rd party

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react"
+import axios from 'axios';
 
 type Story = {
   objectID: number;
@@ -99,12 +100,11 @@ const App = () => {
   const handleFetchStories = React.useCallback(async () => {  
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios.get(url)
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() =>
