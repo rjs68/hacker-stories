4a3387ec8475471b39625aacb78a069c643f49ab
Rhys Stephens <rhys.s@logically.ai>
2023-09-15 15:41:50 +0100

    Styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,83 @@
 import React, { Dispatch, SetStateAction, useCallback, useEffect, useReducer, useState } from "react"
 import axios from 'axios';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  width: 100vw;
+  padding: 20px;
+
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.li`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span<{width: string}>`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  a {
+    color: inherit;
+  }
+
+  width: ${(props: { width: string }) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+  font-size: 24px;
+`;
 
 type Story = {
   objectID: number;
@@ -134,8 +212,8 @@ const App = () => {
   };
 
   return (
-    <>
-      <h1>My Hacker Stories</h1>
+    <StyledContainer>
+      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
 
       <SearchForm searchTerm={searchTerm} onSearchInput={handleSearchInput} onSearchSubmit={handleSearchSubmit}/>
 
@@ -152,7 +230,7 @@ const App = () => {
       ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory}/>
       )}
-    </>
+    </StyledContainer>
   );
 }
 
@@ -173,23 +251,19 @@ type ItemProps = {
 }
 
 const Item = ({ item, onRemoveItem }: ItemProps) => (
-  <li>
-    <span>
+  <StyledItem>
+    <StyledColumn width="40%">
       <a href={item.url}>{item.title}</a>
-    </span>
-    <br />
-    <span>{item.author}</span>
-    <br />
-    <span>{item.num_comments}</span>
-    <br />
-    <span>{item.points}</span>
-    <br />
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
+    </StyledColumn>
+    <StyledColumn width="30%">{item.author}</StyledColumn>
+    <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+    <StyledColumn width="10%">{item.points}</StyledColumn>
+    <StyledColumn width="10%">
+      <StyledButtonSmall type="button" onClick={() => onRemoveItem(item)}>
         Dismiss
-      </button>
-    </span>
-  </li>
+      </StyledButtonSmall>
+    </StyledColumn>
+  </StyledItem>
 );
 
 type InputWithLabelProps = {
@@ -211,9 +285,9 @@ const InputWithLabel = (
     isFocused
   }: InputWithLabelProps) => (
   <>
-    <label htmlFor={id}>{children}</label>
+    <StyledLabel htmlFor={id}>{children}</StyledLabel>
     &nbsp;
-    <input id={id}
+    <StyledInput id={id}
       type={type}
       onChange={onInputChange}
       value={value}
@@ -233,18 +307,18 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit
 }: SearchFormProps) => (
-  <form onSubmit={onSearchSubmit}>
+  <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={onSearchInput}>
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button
+    <StyledButtonLarge
       type="submit"
       disabled={!searchTerm}
     >
       Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+  </StyledSearchForm>
 )
 
 export default App
